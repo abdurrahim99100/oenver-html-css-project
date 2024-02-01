486df07667c6d98fa5d5f4d3bdbba77664314aad
Abdur Rahim <abdurrahim99100@gmail.com>
2024-02-01 22:16:14 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// html file location change about;
+document.getElementById('btn-home').addEventListener('click', function () {
+    window.location.href = 'index.html';
+});
+
+// html file location change about;
+document.getElementById('btn-about').addEventListener('click', function () {
+    window.location.href = 'about.html';
+});
+
+// html file location change services;
+document.getElementById('btn-services').addEventListener('click', function () {
+    window.location.href = 'services.html';
+});
+
+// html file location change services;
+document.getElementById('btn-project').addEventListener('click', function () {
+    window.location.href = 'project.html';
+});
+
+document.getElementById('btn-contact').addEventListener('click', function () {
+    window.location.href = 'contact.html';
+});
